1a4fc216870314b0800e4bac685021edbf97b25b
Chris Allen <chrisallen@sr-0e975f26.wireless.usm.maine.edu>
2018-05-02 20:27:33 -0400

    Cloud Functions for Firebase FireStore Database, articles added to db every hour due to app engine cron job

--- functions/index.js
+++ functions/index.js
@@ -11,10 +11,76 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//import axios from 'axios';
+//import * as firebase from 'firebase';
 
-var functions = require('firebase-functions');
+
+
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    apiKey: "AIzaSyDf3zss9lTnOZLlfkPrTSHzL7oHTZplR94",
+    credential: admin.credential.applicationDefault(),
+    authDomain: "newsbalance-4543f.firebaseapp.com",
+    databaseURL: "https://newsbalance-4543f.firebaseio.com",
+    projectId: "newsbalance-4543f",
+    storageBucket: "newsbalance-4543f.appspot.com",
+    messagingSenderId: "997667764170"
+});
+
+require('firebase/firestore');
 
 exports.hourly_job =
   functions.pubsub.topic('hourly-tick').onPublish((event) => {
-    console.log("This job is ran every hour!")
-  });
+  
+
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('4e76960e84ff466ebb41a90ebc86a625');
+
+
+
+
+var db = admin.firestore();
+  var today = new Date();
+  var yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  var toDate = '' + today.getFullYear() + '-' + today.getMonth() + 1 + '-' + today.getDate();
+  var yesterDate = '' + yesterday.getFullYear() + '-' + yesterday.getMonth() + 1 + '-' + yesterday.getDate();
+  newsapi.v2.everything({
+  	q: 'trump OR economy OR politics OR government OR war OR missile OR health care OR immigration OR racism OR sexism OR united states OR white house OR president',
+  	domains: 'dailycaller.com,huffingtionpost.com,yahoo.com/news/politics/,bloomberg.com/politics,slate.com,theblaze.com',
+  	from: yesterDate,
+  	to: toDate,
+  	languege: 'en',
+  	sortBy: 'publishedAt',
+  	pageSize: 100
+  }).then(res => {
+  	console.log(res);
+  	const articles = res.articles;
+  	for(var i = 0; i < articles.length; i++) {
+  		docName = articles[i].author + articles[i].source.name + articles[i].publishedAt;
+  		db.collection('articles').doc(docName).set(articles[i],{merge: true});
+    }
+	});
+});
+
+exports.addScore = functions.firestore
+	.document('articles/{docId}')
+	.onCreate((snap, context) => {
+	var docRef = snap.ref;
+	var data = snap.data();
+	if(data.urlToImage == null){
+		return docRef.delete();
+	}
+	else {
+		return docRef.set({
+			lScore: 0,
+			rScore: 0,
+			bScore: 0,
+			score: 2 }, {merge: true});
+	}
+});
+
+    		
